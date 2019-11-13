bf77bf86f9944d57db657e96d9a11e63c5a97334
manoj <manoj.sarwate@ssiedu.in>
2019-11-13 18:56:59 +0530

    13-11-2019

--- src/java/TaxCalculator.java
+++ src/java/TaxCalculator.java
@@ -14,7 +14,31 @@ public class TaxCalculator extends HttpServlet {
         //step-1 (reads the request)
         String s1=request.getParameter("t1");
         String s2=request.getParameter("t2");
+        String s3=request.getParameter("city");
+        //String s4=request.getParameter("assets"); 
+        String s[]=request.getParameterValues("assets");
+        String s4=request.getParameter("c1");
+        String s5=request.getParameter("r1");
         
+        int ptax=0;
+        if(s5.equals("salary")){
+            ptax=2000;
+        }else{
+            ptax=3000;
+        }
+                
+        
+        int nriCharges=0;
+        if(s4!=null){
+            nriCharges=5000;
+        }
+        
+        
+        
+        int wtax=0;
+        if(s!=null){
+            wtax=s.length*1000;
+        }
         //step-2 (processes the request)
         int tax=0;
         int income=Integer.parseInt(s1);
@@ -31,7 +55,7 @@ public class TaxCalculator extends HttpServlet {
         PrintWriter out=response.getWriter();
         out.println("<html>");
         out.println("<body bgcolor=gray>");
-        out.println("<h2>Income Tax Dept</h2>");
+        out.println("<h2>Income Tax Dept , "+s3+"</h2>");
         out.println("<hr>");
         out.println("<h3>Thanks For Visiting Us.<h3>");
         out.println("<table border=2>");
@@ -43,6 +67,19 @@ public class TaxCalculator extends HttpServlet {
         out.println("<td>Tax</td>");
         out.println("<td>"+tax+"</td>");
         out.println("</tr>");
+        out.println("<tr>");
+        out.println("<td>W.Tax</td>");
+        out.println("<td>"+wtax+"</td>");
+        out.println("</tr>");
+        out.println("<tr>");
+        out.println("<td>NRI.Tax</td>");
+        out.println("<td>"+nriCharges+"</td>");
+        out.println("</tr>");
+        out.println("<tr>");
+        out.println("<td>PR.Tax</td>");
+        out.println("<td>"+ptax+"</td>");
+        out.println("</tr>");
+        
         out.println("</table>");
         out.println("<hr>");
         out.println("<marquee>Pay Your Taxes Before 31st March<marquee>");
--- web/details.jsp
+++ web/details.jsp
@@ -4,9 +4,30 @@
         <hr>
         <form action="TaxCalculator" method="get">   
             <pre>
+            PAN-NO  <input type="password" name="pn"/>
             Income  <input type="text" name="t1"/>
+            BDate   <input type="date" name="dt"/>
+            Source  Salary      <input type="radio" name="r1" value="salary" checked="checked"/>  
+                    Business    <input type="radio" name="r1" value="business"/>  
             Age     <input type="text" name="t2"/>
+            NRI     <input type="checkbox" name="c1" value="yes"/>
+            City    <select name="city">
+                        <option>Bhopal</option>
+                        <option>Chennai</option>
+                        <option>Delhi</option>
+                        <option>Indore</option>    
+                    </select>
+            Assets  <select name="assets" multiple="multiple">
+                    <option>Car</option>
+                    <option>Flat</option>
+                    <option>Gold</option>
+                    <option>Diamonds</option>
+                    <option>Stocks</option>
+                    <option>Plot</option>
+                    </select>
+            JobDesc <textarea rows="5" cols="20" name="ta"></textarea>
                     <input type="submit" value="FindTax"/>
+                    <input type="reset" value="Reset"/>
             </pre>
         </form>
         <hr>
